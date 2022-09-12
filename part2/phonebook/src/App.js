0d61ed99fe356ee1bf1fe5541e21208f2457feee
nicolastd3 <nico_947quil@hotmail.com>
2022-09-12 20:52:53 -0300

    step5 done

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,5 +1,35 @@
 import { useState } from 'react'
 
+const Filter = ({inputValue, onChangeValue, text}) => <div>{text} <input value={inputValue} onChange={onChangeValue} /></div>
+
+const PersonForm = ({onFormSubmit, onNameChange, nameInputValue, onNumberChange, numberInputValue, buttonText}) =>{
+  return(
+    <div>
+    <form onSubmit={onFormSubmit}>
+      <div> Name <input value={nameInputValue} onChange={onNameChange} /></div>
+
+      <div> Number <input value={numberInputValue} onChange={onNumberChange} /></div>
+      <div>
+        <button type="submit">{buttonText}</button>
+      </div>
+
+    </form>
+    </div>
+  )
+}
+
+const RenderPersons = ({persons,stringFilter})=>{
+  return(
+    persons.map((person) => {
+      if(person.name.toLowerCase().startsWith(stringFilter.toLowerCase()))
+        return(<RenderPerson key={person.name} name={person.name} number={person.number}/>)
+    }
+    )
+  )
+}
+
+const RenderPerson = ({name, number}) => <div> {name} {number}</div>
+
 const App = () => {
   const [persons, setPersons] = useState([
     {
@@ -9,6 +39,10 @@ const App = () => {
     {
       name: 'Ada Lovelace',
       number: '11-4133-8894'
+    },
+    {
+      name: 'Nicolas Buszczak',
+      number: '11-5124-8956'
     }
 
   ])
@@ -30,7 +64,7 @@ const App = () => {
   }
 
 
-  const addData = (event) => {
+  const addDataToList = (event) => {
     event.preventDefault()
     const nameObject = {
       name: newName,
@@ -56,27 +90,17 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
-      <form onSubmit={addData}>
-        <div>
-          filter shown with: <input value={newNameFilter} onChange={handleNameFilterChange} />
-        </div>
-        <h2>Add a new person</h2>
-        <div>
-          name: <input value={newName} onChange={handleNameChange} />
-        </div>
-        <div>
-          number: <input value={newNumber} onChange={handleNumberChange} />
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
+      <Filter inputValue={newNameFilter} onChangeValue={handleNameFilterChange} text={"Filter shown with:"} />
+      <h2>Add a new person</h2>
+      <PersonForm 
+        onFormSubmit={addDataToList}
+        onNameChange={handleNameChange}
+        nameInputValue={newName}
+        onNumberChange={handleNumberChange}
+        numberInputValue={newNumber}
+        buttonText='Add'/>  
       <h2>Numbers</h2>
-      {persons.map((person) => {
-        if(person.name.toLowerCase().startsWith(newNameFilter.toLowerCase()))
-          return(<div key={person.name}> {person.name} {person.number}</div>)
-      }
-      )}
+      <RenderPersons persons={persons} stringFilter={newNameFilter} />
     </div>
   )
 }
