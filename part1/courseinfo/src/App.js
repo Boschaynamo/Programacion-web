87819875f8da49c4442b4c58ee9e71a70f28dc53
nicolastd3 <nico_947quil@hotmail.com>
2022-10-04 00:22:55 -0300

    fix overwrited courseinfo error

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,46 +1,53 @@
-import { useState } from 'react'
-
-const Button = ({ handleClick, text }) => <button onClick={handleClick}>{text}</button>
-
-const History = (props) => {
-  if (props.allClicks.length === 0) {
-    return (
-      <div>
-        The app is used by pressing the buttons
-      </div>
-    )
-  }
+const Header = (props) => {
+  //console.log(props)
+  return <h1>{props.course}</h1>
+}
+const Part = (props) => {
   return (
-    <div>
-      Button press history: {props.allClicks.join('-')}
-    </div>
+    <p>
+      {props.part} {props.exercises}
+    </p>
   )
 }
-
+const Content = (props) => {
+  return (
+    <>
+      <Part part={props.arrayparts[0].name} exercises={props.arrayparts[0].exercises} />
+      <Part part={props.arrayparts[1].name} exercises={props.arrayparts[1].exercises} />
+      <Part part={props.arrayparts[2].name} exercises={props.arrayparts[2].exercises} />
+    </>
+  )
+}
+const Total = (props) => {
+  return (
+    <p> Number of exercises {props.arrayparts[0].exercises}+{props.arrayparts[1].exercises}+{props.arrayparts[2].exercises}</p>
+  )
+}
 const App = () => {
-  const [left, setLeft] = useState(0)
-  const [right, setRight] = useState(0)
-  const [allClicks, setAll] = useState([])
-
-
-  const handleLeftClick = () => {
-    setAll(allClicks.concat('L'))
-    setLeft(left + 1)
-  }
-
-  const handleRightClick = () => {
-    setAll(allClicks.concat('R'))
-    setRight(right + 1)
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
   }
 
   return (
     <div>
-      {left}
-      <Button handleClick={handleLeftClick} text='left button' />
-      <Button handleClick={handleRightClick} text='right button' />
-      {right}
-      <History allClicks={allClicks} />
-    </div>
+      <Header course={course.name} />
+      <Content arrayparts={course.parts} />
+      <Total arrayparts={course.parts} />
+    </div >
   )
 }
 
